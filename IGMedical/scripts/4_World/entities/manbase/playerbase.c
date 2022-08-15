e71dccd465b711343d78002d64a890ad3b00115f
Michael Atkins <email@imperiumxvii.co.uk>
2022-08-15 19:20:25 +0100

    Actual fix for actions not working

--- IGMedical/scripts/4_World/entities/manbase/playerbase.c
+++ IGMedical/scripts/4_World/entities/manbase/playerbase.c
@@ -1,6 +1,7 @@
 modded class PlayerBase extends ManBase {
 	
 	override void SetActions(out TInputActionMap InputActionMap) {
+		super.SetActions(InputActionMap);
 		AddAction(ActionPackWound, InputActionMap);
 	}
 
